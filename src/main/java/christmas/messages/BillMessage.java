7b1c84321f695d8bb9b3cb927d33df4c69d5bcbb
jaegyeongkim <dongji_11@naver.com>
2023-11-11 18:51:23 +0900

    feat: 메시지에 관련된 클래스 구현

---
+++ src/main/java/christmas/messages/BillMessage.java
@@ -0,0 +1,12 @@
+package christmas.messages;
+
+public class BillMessage {
+    private final static String ORDER_MENU_MESSAGE = "주문 메뉴";
+    private final static String BEFORE_DISCOUNT_MESSAGE = "할인 전 총주문 금액";
+    private final static String GIVEAWAY_MESSAGE = "증정 메뉴";
+    private final static String BENEFITS_LIST_MESSAGE = "혜택 내역";
+    private final static String TOTAL_BENEFITS_MESSAGE ="총혜택 금액";
+    private final static String AFTER_COUNT_MESSAGE = "할인 후 예상 결제 금액";
+    private final static String EVENT_BADGE_MASSAGE = "12월 이벤트 배지";
+
+}
---
+++ src/main/java/christmas/messages/ErrorMessages.java
@@ -0,0 +1,7 @@
+package christmas.messages;
+
+public class ErrorMessages {
+    public final static String INVALID_DAY_ERROR = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public final static String INVALID_ORDER_ERROR = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
+}
