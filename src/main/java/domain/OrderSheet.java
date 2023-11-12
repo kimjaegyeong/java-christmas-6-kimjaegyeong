05a8b7c4b467643e7e4fbb4fb3b9b8455c527702
jaegyeongkim <dongji_11@naver.com>
2023-11-12 20:12:15 +0900

    feat: 할인메뉴의 개수를 세는 기능 구현

--- src/main/java/domain/OrderSheet.java
+++ src/main/java/domain/OrderSheet.java
@@ -1,10 +1,34 @@
 package domain;
 
-public class OrderSheet {
-    private final Menu menu;
-    // menu와 category를 한꺼번에 관리하는 클래스 1개 생성 , 그 클래스와 갯수를 hashmap 으로 관리
-    public OrderSheet(Menu menu) {
-        this.menu = menu;
+import domain.date.Day;
+import java.util.List;
 
+public class OrderSheet {
+    private final List<Menu> menus;
+    private final Day day;
+
+    // menu와 category를 한꺼번에 관리하는 클래스 1개 생성 , 그 클래스와 갯수를 hashmap 으로 관리
+    public OrderSheet(List<Menu> menus, Day day) {
+        this.menus = menus;
+        this.day = day;
+    }
+
+    public List<Menu> getMenus() {
+        return menus;
+    }
+
+    public int getDay() {
+        return day.getDay();
+    }
+
+    public int countMenu(Category category) {
+        return (int) menus.stream().filter(menu -> contains(category, menu)).count();
+    }
+
+    public boolean contains(Category category, Menu menu) {
+        if (Category.of(menu).equals(category)) {
+            return true;
+        }
+        return false;
     }
 }
