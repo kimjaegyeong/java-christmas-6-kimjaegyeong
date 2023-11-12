1f26ca3e2050ee236b3c37b622a4ce9461275e7f
jaegyeongkim <dongji_11@naver.com>
2023-11-12 20:49:56 +0900

    refactor: 특별 이벤트 대상인지 구분하는 기능 구현

--- src/main/java/domain/event/SpecialEvent.java
+++ src/main/java/domain/event/SpecialEvent.java
@@ -1,13 +1,31 @@
 package domain.event;
 
-import java.util.Arrays;
-import java.util.List;
+import domain.date.SpecialStar;
+import domain.discount.Discount;
+import domain.discount.SpecialDiscount;
 
 public class SpecialEvent implements Event {
+    private final static int DISCOUNT = 1000;
+    private Discount discount;
+    private static SpecialStar specialStar;
+    private SpecialEvent(int day){
+        discount = new SpecialDiscount(day);
+    }
+
+    public static SpecialEvent create(int day){
+        if(containsEvent(day)){
+            return new SpecialEvent(day);
+        }
+        return null;
+    }
+
+    private static boolean containsEvent(int day) {
+        return new SpecialStar(day).getHasStar();
+    }
 
     @Override
     public int getTotalDiscount() {
-        return 0;
+        return DISCOUNT;
     }
 
     @Override
