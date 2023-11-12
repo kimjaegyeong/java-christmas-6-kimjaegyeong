423fefa12c578db90c26dd75b2dbe85a1aafd4aa
jaegyeongkim <dongji_11@naver.com>
2023-11-12 20:18:54 +0900

    feat: 평일 할인 이벤트 대상인지 구분하는 기능 구현

--- src/main/java/domain/event/DayEvent.java
+++ src/main/java/domain/event/DayEvent.java
@@ -1,11 +1,36 @@
 package domain.event;
 
+import domain.Category;
+import domain.OrderSheet;
+import domain.date.DayOfWeek;
+import domain.date.WeekType;
+import domain.discount.Discount;
+import domain.discount.WeekDiscount;
 import java.util.List;
 
-public class DayEvent implements Event{
+public class DayEvent implements Event {
+    private Discount discount;
+    private int count;
+
+    private final static Category category = Category.DESSERT;
+    private DayEvent(OrderSheet orderSheet) {
+        discount = new WeekDiscount(orderSheet.countMenu(category));
+    }
+
+    public static DayEvent create(OrderSheet orderSheet) {
+        if (containsEvent(orderSheet)) {
+            return new DayEvent(orderSheet);
+        }
+        return null;
+    }
+
+    public static boolean containsEvent(OrderSheet orderSheet){
+        return WeekType.of(DayOfWeek.of(orderSheet.getDay())) == WeekType.WEEKDAY;
+    }
+
     @Override
     public int getTotalDiscount() {
-        return 0;
+        return discount.getDiscountPrice();
     }
 
     @Override
@@ -17,4 +42,5 @@ public class DayEvent implements Event{
     public List<Event> getEvents() {
         return null;
     }
+
 }
