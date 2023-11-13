d5b8ac4bb37acbb036ef574f40f9d85b07b20540
jaegyeongkim <dongji_11@naver.com>
2023-11-13 16:15:05 +0900

    feat: 문자열을 정수형으로 바꾸는 기능 구현

--- src/main/java/utils/Converter.java
+++ src/main/java/utils/Converter.java
@@ -5,4 +5,8 @@ public class Converter {
     public static String[] splitByDelimiter(String input, String delimiter){
         return input.split(delimiter);
     }
+
+    public static int stringToInteger(String input){
+        return Integer.parseInt(input);
+    }
 }
