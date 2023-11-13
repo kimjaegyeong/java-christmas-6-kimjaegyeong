7202011420ed871de02fc90ce8203e98797dead3
jaegyeongkim <dongji_11@naver.com>
2023-11-13 11:30:45 +0900

    feat: 메뉴-개수 쌍을 구하는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,6 +7,7 @@ import utils.Converter;
 public class InputView {
     private final static int MENU_AND_COUNT = 2;
     private final static String DELIMITER = "-";
+    private final static String SEPARATOR = ",";
 
     public String inputVisitDay() {
         String input = Console.readLine();
@@ -18,6 +19,13 @@ public class InputView {
         String input = Console.readLine();
         validationMenuAndCount(input);
         return input;
+    }
+
+    public void separateMenu(String input){
+        String[] menus = Converter.splitByDelimiter(input,SEPARATOR);
+        for(String menu : menus){
+            validationMenuAndCount(menu);
+        }
     }
 
     public void validationMenuAndCount(String input) {
