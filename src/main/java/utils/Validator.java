b8cdaf8f5ced8a45b8b4cc9e605e74c7c6ae2b51
jaegyeongkim <dongji_11@naver.com>
2023-11-13 16:23:10 +0900

    refactor: 검증 기능과 입력 뷰 분리

---
+++ src/main/java/utils/Validator.java
@@ -0,0 +1,76 @@
+package utils;
+
+import messages.ErrorMessages;
+
+public class Validator {
+    public static String[] separateMenu(String input, String separator) {
+        return Converter.splitByDelimiter(input, separator);
+    }
+
+    public static void validationMenus(String input, String separator, int count) {
+        String[] menus = separateMenu(input, separator);
+        for (String menu : menus) {
+            validationMenuAndCount(menu, separator, count);
+        }
+    }
+
+    public static void validationMenuAndCount(String input, String delimiter, int count) {
+        if (invalidDelimiter(input, delimiter, count)) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_ORDER_ERROR);
+        }
+        if (invalidMenuAndCount(input, delimiter)) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_ORDER_ERROR);
+        }
+    }
+
+    public static void validationDay(String input) {
+        if (hasSpace(input) || isEmpty(input) || notNumber(input)) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_DAY_ERROR);
+        }
+    }
+
+    public static boolean invalidMenuAndCount(String input, String delimiter) {
+        String[] inputs = splitMenuAndCount(input,delimiter);
+        String menu = inputs[0];
+        String count = inputs[1];
+        if (hasSpace(menu) || isEmpty(menu) || hasSpace(count) || isEmpty(count) || notNumber(count)) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public static String[] splitMenuAndCount(String input, String delimiter) {
+        return Converter.splitByDelimiter(input, delimiter);
+    }
+
+    public static boolean invalidDelimiter(String input, String delimiter, int count) {
+        String[] inputs = splitMenuAndCount(input, delimiter);
+        if (inputs.length < count) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean hasSpace(String input) {
+        if (input.contains(" ")) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isEmpty(String input) {
+        if (input.isBlank()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean notNumber(String input) {
+        if (!input.matches("\\d+")) {
+            return true;
+        }
+        return false;
+    }
+
+}
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,80 +11,13 @@ public class InputView {
 
     public String inputVisitDay() {
         String input = Console.readLine();
-        validationDay(input);
         return input;
     }
 
     public String inputMenuAndCount() {
         String input = Console.readLine();
-        validationMenuAndCount(input);
         return input;
     }
 
-    public void separateMenu(String input){
-        String[] menus = Converter.splitByDelimiter(input,SEPARATOR);
-        for(String menu : menus){
-            validationMenuAndCount(menu);
-        }
-    }
-
-    public void validationMenuAndCount(String input) {
-        if (invalidDelimiter(input)) {
-            throw new IllegalArgumentException(ErrorMessages.INVALID_ORDER_ERROR);
-        }
-        if (invalidMenuAndCount(input)) {
-            throw new IllegalArgumentException(ErrorMessages.INVALID_ORDER_ERROR);
-        }
-    }
-
-    public void validationDay(String input) {
-        if (hasSpace(input) || isEmpty(input) || notNumber(input)) {
-            throw new IllegalArgumentException(ErrorMessages.INVALID_DAY_ERROR);
-        }
-    }
-
-    public boolean invalidMenuAndCount(String input) {
-        String[] inputs = splitMenuAndCount(input);
-        String menu = inputs[0];
-        String count = inputs[1];
-        if (hasSpace(menu) || isEmpty(menu) || hasSpace(count) || isEmpty(count) || notNumber(count)) {
-            return true;
-        }
-        return false;
-    }
-
-
-    public String[] splitMenuAndCount(String input) {
-        return Converter.splitByDelimiter(input, DELIMITER);
-    }
-
-    public boolean invalidDelimiter(String input) {
-        String[] inputs = splitMenuAndCount(input);
-        if (inputs.length < MENU_AND_COUNT) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean hasSpace(String input) {
-        if (input.contains(" ")) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean isEmpty(String input) {
-        if (input.isBlank()) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean notNumber(String input) {
-        if (!input.matches("\\d+")) {
-            return true;
-        }
-        return false;
-    }
 
 }
