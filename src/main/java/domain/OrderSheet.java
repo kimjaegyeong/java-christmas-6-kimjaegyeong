17f61e4beb176ddb53767d7948b4f66566f4d54f
jaegyeongkim <dongji_11@naver.com>
2023-11-13 16:15:32 +0900

    feat: 주문 메뉴와 개수를 함께 관리하도록 OrderMenu 객체 추가

--- src/main/java/domain/OrderSheet.java
+++ src/main/java/domain/OrderSheet.java
@@ -1,20 +1,17 @@
 package domain;
 
 import domain.date.Day;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OrderSheet {
-    private final List<Menu> menus;
+    private OrderMenu orderMenu;
     private final Day day;
 
     // menu와 category를 한꺼번에 관리하는 클래스 1개 생성 , 그 클래스와 갯수를 hashmap 으로 관리
-    public OrderSheet(List<Menu> menus, Day day) {
-        this.menus = menus;
+    public OrderSheet(Day day, OrderMenu orderMenu) {
+        this.orderMenu = orderMenu;
         this.day = day;
-    }
-
-    public List<Menu> getMenus() {
-        return menus;
     }
 
     public int getDay() {
@@ -22,13 +19,32 @@ public class OrderSheet {
     }
 
     public int countMenu(Category category) {
-        return (int) menus.stream().filter(menu -> contains(category, menu)).count();
+        HashMap<Menu, Integer> menus = orderMenu.getOrderMenu();
+        int sum = 0;
+        return menus.entrySet()
+                .stream()
+                .filter(entry -> contains(category, entry.getKey()))
+                .mapToInt(Map.Entry::getValue)
+                .sum();
     }
+
 
     public boolean contains(Category category, Menu menu) {
         if (Category.of(menu).equals(category)) {
             return true;
         }
         return false;
+    }
+
+    public int getTotalPrice() {
+        HashMap<Menu, Integer> menus = orderMenu.getOrderMenu();
+        int totalPrice = 0;
+
+        for (Map.Entry<Menu, Integer> entry : menus.entrySet()) {
+            Menu menu = entry.getKey();
+            Integer value = entry.getValue();
+            totalPrice = totalPrice + menu.getPrice() * value;
+        }
+        return totalPrice;
     }
 }
