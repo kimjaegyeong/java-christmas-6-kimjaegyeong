4b9be074b2a952392b6e0ad6db5cd3704d25ec46
jaegyeongkim <dongji_11@naver.com>
2023-11-14 14:46:56 +0900

    feat: 총혜택 금액, 할인 전 금액, 할인 후 예상금액 계산 기능 구현

--- src/main/java/domain/Bill.java
+++ src/main/java/domain/Bill.java
@@ -1,5 +1,36 @@
 package domain;
 
-public class Bill {
+import domain.event.Event;
+import domain.event.EventRepository;
 
+public class Bill {
+    OrderSheet orderSheet;
+    EventRepository eventRepository;
+
+    public Bill(OrderSheet orderSheet, EventRepository eventRepository) {
+        this.orderSheet = orderSheet;
+        this.eventRepository = eventRepository;
+
+    }
+    public int getPriceBeforeDiscount(){
+        return orderSheet.getTotalPrice() ;
+    }
+
+    public int getPriceAfterDiscount(EventRepository eventRepository){
+        return orderSheet.getTotalPrice() -  calculateTotalDiscount(eventRepository);
+    }
+    public int calculateTotalDiscount(EventRepository eventRepository){
+        int totalDiscount = 0;
+        for (Event event : eventRepository.getEvents()) {
+            totalDiscount = event.getTotalDiscount() ;
+        }
+        return totalDiscount;
+    }
+    public int calculateTotalBenefits(EventRepository eventRepository) {
+        int totalBenefits = 0;
+        for (Event event : eventRepository.getEvents()) {
+            totalBenefits = event.getTotalDiscount() + event.getTotalBenefits();
+        }
+        return totalBenefits;
+    }
 }
