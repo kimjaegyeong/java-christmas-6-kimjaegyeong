27666f37bf28de51e31f47ea08e51a87480b3bbc
jaegyeongkim <dongji_11@naver.com>
2023-11-14 10:23:23 +0900

    feat: 방문 날짜를 검증하는 기능 구현

--- src/main/java/domain/date/Day.java
+++ src/main/java/domain/date/Day.java
@@ -1,13 +1,28 @@
 package domain.date;
 
+import messages.ErrorMessages;
+
 public class Day {
+    private final static int START = 1;
+    private final static int END = 31;
     private int day;
 
     public Day(int day) {
+        validationDay(day);
         this.day = day;
+
     }
 
     public int getDay() {
         return day;
+    }
+
+    public void validationDay(int day) {
+        if (day > END) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_DAY_ERROR);
+        }
+        if (day < START) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_DAY_ERROR);
+        }
     }
 }
