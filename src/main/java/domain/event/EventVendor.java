911a1466f8851a3c0712c93040d82be73700ccfb
jaegyeongkim <dongji_11@naver.com>
2023-11-14 11:58:47 +0900

    feat: 이벤트 적용 대상인지 검증하는 기능 구현

--- src/main/java/domain/event/EventVendor.java
+++ src/main/java/domain/event/EventVendor.java
@@ -5,23 +5,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EventVendor {
+    public final static int MIN_TOTAL_PRICE = 10000;
     List<Event> events;
     public EventVendor(OrderSheet orderSheet){
         events = new ArrayList<>();
+        if(checkTotalPrice(orderSheet)){
+            putAll(orderSheet);
+        }
+
+
+    }
+    public List<Event> getEvents(){
+        return events;
+    }
+
+    private void putAll(OrderSheet orderSheet){
         put(ChristmasEvent.create(orderSheet));
         put(DayEvent.create(orderSheet));
         put(WeekendEvent.create(orderSheet));
         put(SpecialEvent.create(orderSheet));
         put(GiveawayEvent.create(orderSheet));
     }
-    public List<Event> getEvents(){
-        return events;
-    }
-
     private void put(Event event){
         if(event==null){
             return;
         }
         events.add(event);
+    }
+
+    public boolean checkTotalPrice(OrderSheet orderSheet){
+        if(orderSheet.getTotalPrice()<MIN_TOTAL_PRICE){
+            return false;
+        }
+        return true;
     }
 }
