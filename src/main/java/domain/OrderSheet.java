c6af07f32ab7082864293f1e06f5341325e499f6
jaegyeongkim <dongji_11@naver.com>
2023-11-14 11:22:22 +0900

    refactor: 불필요한 메소드 이동, 메소드명 변경

--- src/main/java/domain/OrderSheet.java
+++ src/main/java/domain/OrderSheet.java
@@ -8,7 +8,6 @@ public class OrderSheet {
     private OrderMenu orderMenu;
     private final Day day;
 
-    // menu와 category를 한꺼번에 관리하는 클래스 1개 생성 , 그 클래스와 갯수를 hashmap 으로 관리
     public OrderSheet(Day day, OrderMenu orderMenu) {
         this.orderMenu = orderMenu;
         this.day = day;
@@ -18,33 +17,26 @@ public class OrderSheet {
         return day.getDay();
     }
 
-    public int countMenu(Category category) {
+    public int countMenuIn(Category category) {
         HashMap<Menu, Integer> menus = orderMenu.getOrderMenu();
         int sum = 0;
         return menus.entrySet()
                 .stream()
-                .filter(entry -> contains(category, entry.getKey()))
+                .filter(entry -> containsIn(category, entry.getKey()))
                 .mapToInt(Map.Entry::getValue)
                 .sum();
     }
 
 
-    public boolean contains(Category category, Menu menu) {
+    public boolean containsIn(Category category, Menu menu) {
         if (Category.of(menu).equals(category)) {
             return true;
         }
         return false;
     }
 
-    public int getTotalPrice() {
-        HashMap<Menu, Integer> menus = orderMenu.getOrderMenu();
-        int totalPrice = 0;
-
-        for (Map.Entry<Menu, Integer> entry : menus.entrySet()) {
-            Menu menu = entry.getKey();
-            Integer value = entry.getValue();
-            totalPrice = totalPrice + menu.getPrice() * value;
-        }
-        return totalPrice;
+    public int getTotalPrice(){
+        return orderMenu.calculateTotalPrice();
     }
+
 }
