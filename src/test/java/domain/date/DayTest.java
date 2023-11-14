6c83c6582882b426617b741e4878acf04536c6b9
jaegyeongkim <dongji_11@naver.com>
2023-11-14 10:23:34 +0900

    feat: 방문 날짜를 검증하는 기능 테스트

--- src/test/java/domain/date/DayTest.java
+++ src/test/java/domain/date/DayTest.java
@@ -1,7 +1,9 @@
 package domain.date;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import messages.ErrorMessages;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +15,10 @@ public class DayTest {
     public void createDay(){
         Day day = new Day(3);
         assertThat(day.getDay()).isEqualTo(3);
+    }
+    @DisplayName("잘못된 방문 날짜 입력 테스트")
+    @Test
+    public void invalidVisitDay(){
+        assertThatThrownBy(()->new Day(32)).isInstanceOf(IllegalArgumentException.class);
     }
 }
