e8878e4e598d978b563db88d5e2ed2ce2eedf9b2
jaegyeongkim <dongji_11@naver.com>
2023-11-15 09:47:04 +0900

    feat: 고객에게 뱃지 부여 기능 구현

--- src/main/java/domain/Customer.java
+++ src/main/java/domain/Customer.java
@@ -1,5 +1,17 @@
 package domain;
 
+import domain.event.EventRepository;
+
 public class Customer {
 
+    OrderSheet orderSheet;
+    Badge badge;
+    public Customer(OrderSheet orderSheet){
+     this.orderSheet = orderSheet;
+    }
+
+    public void getBadge(EventRepository eventRepository){
+        Bill bill= new Bill(orderSheet, eventRepository);
+        badge = Badge.of(bill.totalBenefits(eventRepository));
+    }
 }
