2edcbc7f4fd0b94ce428a47304ba0b9db81b3e70
jaegyeongkim <dongji_11@naver.com>
2023-11-15 09:46:42 +0900

    feat: 이벤트에 관한 메시지 추가

---
+++ src/main/java/messages/EventMessage.java
@@ -0,0 +1,10 @@
+package messages;
+
+public class EventMessage {
+    public final static String CHRISTMAS_EVENT_MESSAGE = "크리스마스 디데이 할인";
+    public final static String DAY_EVENT_MESSAGE = "평일 할인";
+    public final static String WEEKEND_EVENT_MESSAGE = "주말 할인";
+    public final static String SPECIAL_EVENT_MESSAGE= "특별 할인";
+    public final static String GIVEAWAY_EVENT_MESSAGE = "증정 이벤트";
+
+}
