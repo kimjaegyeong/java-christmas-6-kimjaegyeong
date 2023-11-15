5ed13e97a2a2c38cdd75617545a1e132638e184c
jaegyeongkim <dongji_11@naver.com>
2023-11-15 12:16:34 +0900

    feat: 배지를 얻는 기능과 반환하는 기능 분리

--- src/main/java/domain/Customer.java
+++ src/main/java/domain/Customer.java
@@ -6,12 +6,17 @@ public class Customer {
 
     OrderSheet orderSheet;
     Badge badge;
-    public Customer(OrderSheet orderSheet){
+    public Customer(OrderSheet orderSheet, EventRepository eventRepository){
      this.orderSheet = orderSheet;
+     createBadge(eventRepository);
     }
 
-    public void getBadge(EventRepository eventRepository){
+    public void createBadge(EventRepository eventRepository){
         Bill bill= new Bill(orderSheet, eventRepository);
         badge = Badge.of(bill.totalBenefits(eventRepository));
+    }
+
+    public String getBadge(){
+        return badge.getName();
     }
 }
