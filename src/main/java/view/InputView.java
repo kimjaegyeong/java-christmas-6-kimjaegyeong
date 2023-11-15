0101433e2485cdf6e076e1acedf8796e7ecd9e72
jaegyeongkim <dongji_11@naver.com>
2023-11-15 12:14:56 +0900

    feat: 안내 메시지 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,8 +1,7 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
-import messages.ErrorMessages;
-import utils.Converter;
+import messages.InformationMassage;
 
 public class InputView {
     private final static int MENU_AND_COUNT = 2;
@@ -10,13 +9,19 @@ public class InputView {
     private final static String SEPARATOR = ",";
 
     public String inputVisitDay() {
+        System.out.println(InformationMassage.WELCOME_MESSAGE);
+        System.out.println(InformationMassage.INPUT_VISIT_DAY_MESSAGE);
         String input = Console.readLine();
         return input;
     }
 
     public String inputMenuAndCount() {
+        System.out.println(InformationMassage.INPUT_MENU_MESSAGE);
         String input = Console.readLine();
         return input;
+    }
+    public void printSentence(String notice,int input){
+        System.out.printf(notice,input);
     }
 
 
